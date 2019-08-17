88c69a3fa635dd6b58c639b5a47ff5c254ef5064
Dogeek <Dogeek@users.noreply.github.com>
2019-08-17 14:37:27 +0200

    wrong folder lol

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-from Game import *
+from lib.utils import sanitized_input
+from lib.game import Game
+
+
+def main():
+    print("Welcome to rpg-text!")
+    print("Please enter your character's name.")
+    p_name = sanitized_input("> ", error_msg="")
+    print("Please enter your desired job (commoner, spellcaster, warrior).")
+    p_job = sanitized_input("> ", valid_input=('commoner', 'spellcaster', 'warrior'))
+    game = Game(p_name, p_job)
+    while True:
+        game.next_turn()
+
+
+if __name__ == '__main__':
+    main()
---
+++ saves/saves.md
@@ -0,0 +1,3 @@
+Saves are stored in the JSON format.
+
+See lib/saveload.py for the save system and how it is managed.
