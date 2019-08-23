c99d6873396fce1fbae1457ce2bce44f7b416aad
Dogeek <Dogeek@users.noreply.github.com>
2019-08-23 12:34:10 +0200

    GUI work

--- main-gui.py
+++ main-gui.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkgui.window import Window
 import io
 from rpglib.game import Game
+from rpglib.utils import parse_dice_format
 
 
 game = Game()
@@ -9,16 +10,37 @@ game = Game()
 
 def loop():
     global game
-    while True:
-        game.next_turn()
+    game.next_turn()
 
+
+def new_game():
+    global game
+    p_name = "Dogeek"
+    p_job = "warrior"
+    game.player.name = p_name
+    game.player.job = p_job
+    game.player.health_rolls.append(parse_dice_format(game.player.job.hp_die))
+    game.player.mana_rolls.append(parse_dice_format(game.player.job.mp_die))
+    game.player.stats.randomize()
+    game.player.health = game.player.max_health
+
+
+def on_click(event):
+    print("Clicked!")
+
+
+new_game()
 
 root = tk.Tk()
-window = Window(root)  # , loop=loop)
+window = Window(root, loop=loop)
 window.pack()
 
 with io.open("assets/screens/game", encoding='utf-8') as f:
     for i, line in enumerate(f):
-        window.print(line, (0, i * 20), persistent=True)
+        window.print(line, (0, i))
+
+window.print(game.player.name, (50, 1))
+
+# window.button('CLICK', (10, 4), on_click)
 
 root.mainloop()
--- tkgui/window.py
+++ tkgui/window.py
@@ -7,37 +7,66 @@ class TkIOWrapper(_io.TextIOWrapper):
 
 
 class Window(tk.Frame):
-    def __init__(self, master=None, size=(1024, 768), loop=None):
+    def __init__(self, master=None, width=80, aspect_ratio=4/3, loop=None):
         super().__init__()
         self.master = master
 
-        self.canvas = tk.Canvas(self, width=size[0], height=size[1], bg='black')
+        self.master.resizable(False, False)
+
+        self.width = width
+        self.height = int(round(width / aspect_ratio, 0))
+        w = width * 10
+        h = self.height * 10
+
+        self.canvas = tk.Canvas(self, width=w, height=h, bg='black')
         self.canvas.pack()
         self.items = []
-        self.persistent_items = []
+
+        for x in range(self.width + 1):
+            items = []
+            for y in range(self.height):
+                items.append(self.canvas.create_text((x * 10, y * 20), text=" ", fill="white",
+                                                     font=('DejaVu Sans Mono', ), anchor=tk.NW))
+            self.items.append(items)
+
         self._loop_callable = loop
+        self.did_action = False
         self.loop()
 
-    def print(self, text, position, persistent=False, **kwargs):
-        item = self.canvas.create_text(position, text=text, fill='white', font=('DejaVu Sans Mono', ), anchor=tk.NW,
-                                       **kwargs)
-        if persistent:
-            self.persistent_items.append(item)
-        else:
-            self.items.append(item)
-        return item
+    def print(self, text, position, **kwargs):
+        x_start, y_start = position
+        for i, character in enumerate(text):
+            print(x_start+i, y_start)
+            x = min(x_start + i, self.width)
+            y = min(y_start, self.height)
+            iid = self.items[x][y]
+            self.canvas.itemconfigure(iid, text=character, **kwargs)
 
     def button(self, text, position, callback):
-        t_id = self.print(text, position, fillactive='yellow')
-        self.canvas.tag_bind(t_id, '<Button-1>', callback)
+        def highlight(evt):
+            self.canvas.itemconfigure(tag_name, fill='yellow')
+
+        def de_highlight(evt):
+            self.canvas.itemconfigure(tag_name, fill='white')
+
+        tag_name = f'button-{position[0]}{position[1]}'
+        self.print(text, position, tags=tag_name)
+        self.canvas.tag_bind(tag_name, '<Button-1>', callback)
+        self.canvas.tag_bind(tag_name, '<Enter>', highlight)
+        self.canvas.tag_bind(tag_name, '<Leave>', de_highlight)
 
     def clear(self):
-        while self.items:
-            i = self.items.pop()
-            self.canvas.delete(i)
+        self.items = []
+
+        for x in range(self.width + 1):
+            items = []
+            for y in range(self.height):
+                items.append(self.canvas.create_text((x * 10, y * 20), text=" ", fill="white",
+                                                     font=('DejaVu Sans Mono', ), anchor=tk.NW))
+            self.items.append(items)
 
     def loop(self):
-        if callable(self._loop_callable):
+        if callable(self._loop_callable) and self.did_action:
             self._loop_callable()
 
         self.after(50, self.loop)
