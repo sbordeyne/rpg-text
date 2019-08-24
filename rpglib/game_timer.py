2e97a69985a2155ab358f06cb0b4c731e5bfd457
Dogeek <Dogeek@users.noreply.github.com>
2019-08-24 15:59:10 +0200

    fix #1 - timer calculations

--- rpglib/game_timer.py
+++ rpglib/game_timer.py
@@ -39,22 +39,57 @@ class GameTimer:
         self.n_turns += n_ticks
 
     @property
+    def game_day(self):
+        return (self.init_time + self.n_turns * Calendar.minutes_per_turn) // \
+               (Calendar.hours_per_day * Calendar.minutes_per_hour)
+
+    @property
+    def month_number(self):
+        return (self.game_day // Calendar.days_per_month) % len(Calendar.months) + 1
+
+    @property
+    def month_name(self):
+        return Calendar.months[self.month_number - 1]
+
+    @property
+    def day_number(self):
+        return self.game_day % Calendar.days_per_month + 1
+
+    @property
+    def day_number_suffix(self):
+        if int(str(self.day_number)[-1]) == 1:
+            return 'st'
+        elif int(str(self.day_number)[-1]) == 2:
+            return 'nd'
+        elif int(str(self.day_number)[-1]) == 3:
+            return 'rd'
+        else:
+            return 'th'
+
+    @property
+    def weekday_name(self):
+        return Calendar.days[(self.day_number - 1) % Calendar.days_per_week]
+
+    @property
+    def year_number(self):
+        return self.init_year + self.game_day // Calendar.days_per_year
+
+    @property
     def date(self):
-        game_day = (self.init_time + self.n_turns * Calendar.minutes_per_turn) // \
-                   (Calendar.hours_per_day * Calendar.minutes_per_hour)
-        month_name = Calendar.months[(game_day // Calendar.days_per_month) % len(Calendar.months)]
-        day_number = game_day % Calendar.days_per_month
-        weekday_name = Calendar.days[day_number % Calendar.days_per_week]
-        year_number = self.init_year + game_day // Calendar.days_per_year
-        return f"{weekday_name} the {day_number} of {month_name}, Year X{year_number} at {self.time}"
+        return f"{self.weekday_name.capitalize()} the {self.day_number}{self.day_number_suffix} of " \
+               f"{self.month_name.capitalize()}, Year X{self.year_number} at {self.time}"
+
+    @property
+    def short_date(self):
+        return f'{self.day_number}-{self.month_number}-X{self.year_number} @ {self.time}'
 
     @property
     def time(self):
-        total_minutes = (self.init_time + self.n_turns * Calendar.minutes_per_turn) // \
+        total_minutes = (self.init_time + self.n_turns * Calendar.minutes_per_turn) % \
                         (Calendar.hours_per_day * Calendar.minutes_per_hour)
         hours = total_minutes % Calendar.minutes_per_hour
         minutes = total_minutes // Calendar.minutes_per_hour
-        return f"{hours}:{minutes}"
+        return f"{hours:02}:{minutes:02}"
 
     def serialize(self):
         return {"n_turns": self.n_turns}
