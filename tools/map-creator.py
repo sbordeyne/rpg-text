5d0421d95a31049473d9e3e3b497f491a2ad7f4e
Dogeek <Dogeek@users.noreply.github.com>
2019-12-16 03:46:09 +0100

    Update map-creator.py

--- tools/map-creator.py
+++ tools/map-creator.py
@@ -4,6 +4,35 @@ import tkinter.filedialog as fd
 import string
 from collections import defaultdict, OrderedDict
 import json
+import os.path
+
+
+HELP_TXT = """
+Map Creator GUI - Tool made by Dogeek for https://github.com/Dogeek/rpg-text
+
+Click on the map to select a cell. You can then edit the cell info
+to your liking.
+
+Click on the save button in the Form once you are done editing the cell info.
+
+Form attributes :
+
+Position - current cell position, read only
+Location Name - a unique name for this location.
+Description - a description of the location shown to the player once he enters.
+NPCs - A space-separated list of character names present in this cell.
+Icon - An icon to represent this location on the map, must be a single character
+Submaps - A space-separated list of submaps to enter from this location.
+Exits - Tick in which directions the player can go from this location.
+Save - Saves the current cell info.
+
+Menubar :
+Save - Saves the map file.
+Load - Loads a map file.
+Help - Show this window.
+
+(c) Dogeek - 2019
+"""
 
 
 def get_avg_charwidth(widget=None, text=None):
@@ -14,7 +43,18 @@ def get_avg_charwidth(widget=None, text=None):
     else:
         font = tkfont.Font(font=widget['font'])
     return sum([font.measure(c) for c in text]) / len(text)
-        
+
+
+class HelpWindow(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.text = tk.Text(self, width=80, height=40)
+        self.vsb = tk.Scrollbar(self, command=self.text.yview)
+        self.text.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
+        self.vsb.pack(side=tk.RIGHT, expand=True, fill=tk.Y)
+        self.text.insert(tk.END, HELP_TXT)
+        self.text.config(state=tk.DISABLED, xscrollcommand=self.vsb.set)
+
 
 class DataSerializer:
     def __init__(self, filename='map.json'):
@@ -26,7 +66,7 @@ class DataSerializer:
         self.filename = fname
         with open(fname, 'w') as f:
             json.dump(self.data, f, indent=4)
-    
+
     def load(self, filename=None):
         fname = filename or self.filename
         self.filename = fname
@@ -82,7 +122,7 @@ class FormView(tk.Frame):
         self.form_entries = OrderedDict()
         self.form_labels = OrderedDict()
         self.form_exits = OrderedDict()
-        self.form_entries_names = ('Location Name', 'Description', 'NPCs', 'Icon')
+        self.form_entries_names = ('Location Name', 'Description', 'NPCs', 'Icon', "Submaps")
         self.exits_names = ('N', 'S', 'E', 'W')
         self.position_variable = tk.StringVar()
         self.position_variable.set('(0, 0)')
@@ -128,6 +168,7 @@ class FormView(tk.Frame):
         self.master.data.data[name]['map_icon'] = self.variables["Icon"].get()
         self.master.data.data[name]['npc'] = self.variables["NPCs"].get().split(' ')
         self.master.data.data[name]['position'] = list(self.master.canvas.map_position)
+        self.master.data.data[name]['submaps'] = self.variables["Submaps"].get().split(' ')
         self.master.data.data[name]['exits'] = [en for e, en in zip(self.exits_var, self.exits_names) if e.get() == 1]
 
     def load(self, x, y):
@@ -136,7 +177,7 @@ class FormView(tk.Frame):
             if val.get('position') == [x, y]:
                 name = key
                 break
-            
+
         if self.master.data.data[name] != {}:
             self.form_entries['Description'].delete('1.0', tk.END)
             self.form_entries["Description"].insert(tk.END, self.master.data.data[name]['description'])
@@ -159,9 +200,12 @@ class MapCreatorGUI(tk.Frame):
         kwargs['master'] = master
         super().__init__(*args, **kwargs)
         self.master = master
+        self.help_window = None
+
         self.menubar = tk.Menu(self.master)
         self.menubar.add_command(label="Save", command=self._on_data_save)
         self.menubar.add_command(label="Load", command=self._on_data_load)
+        self.menubar.add_command(label="Help", command=self._on_help)
         self.master.config(menu=self.menubar)
 
         self.form_frame = FormView(self)
@@ -184,24 +228,39 @@ class MapCreatorGUI(tk.Frame):
         longest_label_name = sorted(self.form_frame.form_entries_names, key=len)[-1]
         margin_width = get_avg_charwidth(self.form_frame.form_labels[longest_label_name],
                                          longest_label_name) * len(longest_label_name)
-        new_width = abs(int(self.winfo_width()) - int(canvw) - margin_width)                   
+        new_width = abs(int(self.winfo_width()) - int(canvw) - margin_width)
         cw = 8.8  # get_avg_charwidth()
         new_width_in_characters = int(new_width // cw)
         for entry in self.form_frame.form_entries_names:
             self.form_frame.form_entries[entry].config(width=new_width_in_characters)
-            
+
     def _on_data_load(self, *args):
         fname = fd.askopenfilename()
+        if not os.path.exists(fname):
+            return
         self.data.load(fname)
         for v in [_ for _ in self.data.data.values() if _]:
             x, y = v['position']
             x *= 16
             y *= 16
             self.canvas.create_rectangle(x, y, x + 16, y + 16, stipple='gray50', fill='grey')
-    
+
     def _on_data_save(self, *args):
         fname = fd.asksaveasfilename()
         self.data.save(fname)
+
+    def _on_help(self, *args):
+        def _on_help_close():
+            self.help_window.destroy()
+            self.help_window = None
+
+        if self.help_window is None:
+            self.help_window = tk.Toplevel(self)
+            self.help_window.title("Help")
+            self.help_window.resizable(False, False)
+            gui = HelpWindow(self.help_window)
+            gui.pack(expand=True, fill=tk.BOTH)
+            self.help_window.protocol('WM_DELETE_WINDOW', _on_help_close)
 
     def loop(self):
         self.after(50, self.loop)
